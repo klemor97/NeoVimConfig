c22842f00c4e704d5fcc1f85f109062345fe0777
klemor97 <klemor97@gmail.com>
2023-12-29 21:30:46 +0200

    html & emmet

--- lua/klemor97/plugins/mason.lua
+++ lua/klemor97/plugins/mason.lua
@@ -9,7 +9,7 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "tsserver" },
+				ensure_installed = { "lua_ls", "clangd", "tsserver", "html", "emmet_ls" },
 			})
 		end,
 	},
@@ -17,8 +17,25 @@ return {
 		"neovim/nvim-lspconfig",
 		config = function()
 			local lspconfig = require("lspconfig")
+
+			local capabilities = vim.lsp.protocol.make_client_capabilities()
+			capabilities.textDocument.completion.completionItem.snippetSupport = true
+
 			lspconfig.lua_ls.setup({})
+			lspconfig.clangd.setup({})
 			lspconfig.tsserver.setup({})
+			lspconfig.html.setup({})
+			lspconfig.emmet_ls.setup({
+        capabilities = capabilities,
+        filetypes = { "html" },
+        init_options = {
+          html = {
+            options = {
+              ["bem.enabled"] = true
+            }
+          }
+        }
+      })
 
 			vim.keymap.set("n", "<leader>i", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
--- lua/klemor97/plugins/treesitter.lua
+++ lua/klemor97/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
   build = ":TSUpdate",
   config = function()
     require("nvim-treesitter.configs").setup({
-      ensure_installed = { "lua", "c", "cpp", "javascript", "typescript" },
+      ensure_installed = { "lua", "c", "cpp", "javascript", "typescript", "html" },
       indent = { enable = true },
       highlight = { enable = true }
     })
