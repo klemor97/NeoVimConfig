d55f24c40ff5446f3a1afb086208b8bc049f1e27
klemor97 <klemor97@gmail.com>
2024-01-12 16:02:17 +0200

    Single file init.lua setup

--- init.lua
+++ init.lua
@@ -49,64 +49,82 @@ vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
   {
-    "dracula/vim", name = "dracula", priority = 1000
+    "dracula/vim",
+    name = "dracula",
+    priority = 1000,
   },
   {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' }
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
   },
   {
     "nvim-tree/nvim-tree.lua",
-	  dependencies = { "nvim-tree/nvim-web-devicons" }
+    dependencies = { "nvim-tree/nvim-web-devicons" },
   },
   {
-    "nvim-treesitter/nvim-treesitter", build = ":TSUpdate"
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
   },
   {
-    "nvim-telescope/telescope.nvim", tag = "0.1.5",
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.5",
     dependencies = { "nvim-lua/plenary.nvim" },
   },
   {
-    "nvim-telescope/telescope-ui-select.nvim"
+    "nvim-telescope/telescope-ui-select.nvim",
   },
   {
-    "williamboman/mason.nvim"
+    "williamboman/mason.nvim",
   },
   {
-    "williamboman/mason-lspconfig.nvim"
+    "williamboman/mason-lspconfig.nvim",
   },
   {
-    "neovim/nvim-lspconfig"
-  }
+    "neovim/nvim-lspconfig",
+  },
+  {
+    "nvimtools/none-ls.nvim",
+  },
+  {
+    "hrsh7th/nvim-cmp",
+  },
+  {
+    "hrsh7th/cmp-nvim-lsp"
+  },
+  {
+    "L3MON4D3/LuaSnip",
+    dependencies = { "saadparwaiz1/cmp_luasnip", "rafamadriz/friendly-snippets" },
+  },
 }
 
 require("lazy").setup(plugins)
 
 require("lualine").setup({
   options = {
-    theme = "dracula"
-  }
+    theme = "dracula",
+  },
 })
 
 require("nvim-tree").setup()
 
 require("nvim-treesitter.configs").setup({
-  ensure_installed = { "lua", "javascript", "typescript", "html", "css" },
+  ensure_installed = { "lua", "javascript", "typescript", "html", "css", "python", "c" },
   indent = { enable = true },
-  highlight = { enable = true }
+  highlight = { enable = true },
 })
 
 require("telescope").setup({
   defaults = {
     file_ignore_patterns = {
-      "node_modules"
-    }
+      "node_modules",
+      "venv"
+    },
   },
   extensions = {
     ["ui-select"] = {
-      require("telescope.themes").get_dropdown({})
-    }
-  }
+      require("telescope.themes").get_dropdown({}),
+    },
+  },
 })
 
 require("telescope").load_extension("ui-select")
@@ -116,27 +134,83 @@ local telescope_builtin = require("telescope.builtin")
 require("mason").setup()
 
 require("mason-lspconfig").setup({
-	ensure_installed = { "lua_ls", "tsserver", "html", "cssls", "emmet_ls" }
+  ensure_installed = { "lua_ls", "tsserver", "html", "cssls", "emmet_ls", "pyright", "clangd" },
 })
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
+local cmp_capabilities = require("cmp_nvim_lsp").default_capabilities()
+
 local lspconfig = require("lspconfig")
-lspconfig.lua_ls.setup({})
-lspconfig.tsserver.setup({})
-lspconfig.html.setup({})
-lspconfig.cssls.setup({})
+lspconfig.lua_ls.setup({
+  capabilities = cmp_capabilities
+})
+lspconfig.tsserver.setup({
+  capabilities = cmp_capabilities
+})
+lspconfig.html.setup({
+  capabilities = cmp_capabilities
+})
+lspconfig.cssls.setup({
+  capabilities = cmp_capabilities
+})
 lspconfig.emmet_ls.setup({
-  capabilities = capabilities,
+  capabilities = { capabilities, cmp_capabilities },
   filetypes = { "html", "css", "javascript" },
   init_options = {
     html = {
       options = {
-        ["bem.enabled"] = true
-      }
-    }
-  }
+        ["bem.enabled"] = true,
+      },
+    },
+  },
+})
+lspconfig.pyright.setup({
+  capabilities = cmp_capabilities
+})
+lspconfig.clangd.setup({
+  capabilities = cmp_capabilities
+})
+
+local null_ls = require("null-ls")
+null_ls.setup({
+  sources = {
+    null_ls.builtins.formatting.stylua,
+    null_ls.builtins.formatting.prettier,
+    null_ls.builtins.formatting.black,
+
+    null_ls.builtins.diagnostics.eslint_d,
+  },
+})
+
+local cmp = require("cmp")
+require("luasnip.loaders.from_vscode").lazy_load()
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      require("luasnip").lsp_expand(args.body)
+    end,
+  },
+  window = {
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
+  },
+  mapping = cmp.mapping.preset.insert({
+    -- ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+    -- ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    -- ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-k>"] = cmp.mapping.select_prev_item(),
+    ["<C-j>"] = cmp.mapping.select_next_item(),
+    ["<Escape>"] = cmp.mapping.abort(),
+    ["<Tab>"] = cmp.mapping.confirm({ select = true }),
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = "luasnip" },
+  }, {
+    { name = "buffer" },
+  }),
 })
 
 -- Set Theme
@@ -146,7 +220,6 @@ vim.cmd("colorscheme dracula")
 
 -- Keymaps
 vim.keymap.set("n", "<leader>t", ":NvimTreeToggle<CR>")
-vim.keymap.set("n", "<leader>af", "gg=G")
 
 vim.keymap.set("n", "<leader>ff", telescope_builtin.find_files, {})
 vim.keymap.set("n", "<leader>fg", telescope_builtin.live_grep, {})
@@ -157,3 +230,6 @@ vim.keymap.set("n", "<leader>i", vim.lsp.buf.hover, {})
 vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
 vim.keymap.set("n", "<leader>gi", vim.lsp.buf.implementation, {})
 vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+
+-- vim.keymap.set("n", "<leader>af", "gg=G")
+vim.keymap.set("n", "<leader>af", vim.lsp.buf.format, {})
